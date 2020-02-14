9139a27a6fc3b5b7bf50272ef919b1ed9ee152da
Konrad Sochacki <sochacki.konrad@gmail.com>
2020-02-14 12:20:11 +0100

    Add SIGN_REPETITION_THRESHOLD

--- camera_controller.py
+++ camera_controller.py
@@ -3,6 +3,7 @@ import os
 import cv2
 from sklearn.decomposition import PCA
 from sklearn.svm import LinearSVC
+from pynput.keyboard import Key, Controller
 
 import constants
 import image_util
@@ -12,10 +13,11 @@ from constants import PROJECT_NAME
 import constants
 
 complete_classifier: CompleteClassifier = recognition_util.open_model('trained_models/complete_classifier_2')
-
+keyboard = Controller()
 
 def start_camera():
     camera = cv2.VideoCapture('http://0.0.0.0:4747/mjpegfeed')
+    occured_sign_list = []
 
     while True:
         (grabbed, frame) = camera.read()
@@ -25,10 +27,20 @@ def start_camera():
 
         try:
             if check_if_applies_to_threshold(black_and_white_image):
-                print("Applies to threshold", complete_classifier.predict_one_image(black_and_white_image), ', ',
+                predicted_sign = complete_classifier.predict_one_image(black_and_white_image)[0]
+                print("Applies to threshold", predicted_sign, ', ',
                       complete_classifier.get_predict_proba_of_image(black_and_white_image)[0])
+                occured_sign_list.append(predicted_sign)
+                if len(occured_sign_list) >= constants.SIGN_REPETITION_THRESHOLD:
+                    if len(set(occured_sign_list)) == 1:
+                        perform_action(predicted_sign)
+                        occured_sign_list.clear()
+                    else:
+                        occured_sign_list.pop(0)
         except ValueError as valueError:
             print('ValueError', valueError)
+        except IndexError as indexError:
+            print('IndexError', indexError)
 
         # print("predicted:", complete_classifier.predict_one_image(black_and_white_image))
         # print("proba:", complete_classifier.get_predict_proba_of_image(black_and_white_image)[0])
@@ -49,6 +61,18 @@ def start_camera():
 def check_if_applies_to_threshold(black_and_white_image):
     return True in [item > constants.CLASSIFICATION_PROBABILITY_THRESHOLD for item in
                     complete_classifier.get_predict_proba_of_image(black_and_white_image)[0]]
+
+
+def perform_action(action: str):
+    print('Performing action', action)
+    if action == 'left':
+        keyboard.press(Key.right)
+        keyboard.release(Key.right)
+    elif action == 'right':
+        keyboard.press(Key.left)
+        keyboard.release(Key.left)
+    else:
+        print('Unrecognized action:', action)
 
 
 # Main script
